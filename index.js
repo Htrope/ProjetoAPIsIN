652290533b765d5b1f348500e6d2a178a44feaa1
Henrique Trope <tropehe12@gmail.com>
2025-08-03 13:25:00 -0300

    src: Desenvolvendo o main da pag principal

--- index.js
+++ index.js
@@ -11,14 +11,28 @@ async function carregarProdutos() {
       const card = document.createElement('div');
       card.classList.add('card-produto');
 
-      card.innerHTML = `
-        <img src="camisa.jpg" alt="${produto.name}">
-        <div class="info-produto">
-          <h3>${produto.name}</h3>
-        <p>${produto.description || 'Sem descrição'}</p>
-        <p class="preco">R$ ${Number(produto.price).toFixed(2)}</p>
-        </div>
-        `;
+     card.innerHTML = `
+  <div class="imagem-produto">
+    <img src="camisa.jpg" alt="${produto.name}">
+
+    <a href="excluirProduto.html?id=${produto.id}">
+      <img src="FrameLixeira.png" alt="Excluir" class="lixeira" />
+    </a>
+
+    <a href="editarProduto.html?id=${produto.id}">
+      <img src="FrameLapis.png" alt="Editar" class="btnEditar" />
+    </a>
+  </div>
+
+  <div class="info-produto">
+    <h3>${produto.name}</h3>
+    <p>${produto.description || 'Sem descrição'}</p>
+    <p class="preco">R$ ${Number(produto.price).toFixed(2)}</p>
+  </div>
+`;
+
+
+
 
       container.appendChild(card);
     });
